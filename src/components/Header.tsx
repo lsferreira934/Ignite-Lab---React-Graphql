a38b0633c378c101001fdd57a4f0ed91930548e3
Leandro <leandro.ferreira@plooral.com>
2022-06-25 01:51:09 -0300

    Resposive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { List } from "phosphor-react";
 import { Logo } from "./Logo";
 
-export function Header(){
-  return(
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+export function Header() {
+  return (
+    <header className="w-full p-5 flex items-center justify-between md:justify-center  bg-gray-700 border-b border-gray-600">
       <Logo />
+      <List size={34} className="block md:hidden"/>
     </header>
   );
-};
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ export function Sidebar() {
   const { data } = useGetLessonsQuery();
 
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="absolute right-0 hidden w-3/5 md:relative md:block md:w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
